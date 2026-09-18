ddcd901db83fe3a05b778af39a57dc72b6f4868f
samratchauhan1 <samrattirwa@gmail.com>
2026-09-18 17:04:14 +0530

    Fix auth service

--- service/auth.js
+++ service/auth.js
@@ -1,7 +1,20 @@
 
 // //ye basically eek diary bana rhe hai statefull
 // //sabse badi prblm is jb bhi ham apna srrver restart krte hai tb hamara ye map khali ho jata hai
+const sessionidUseMap=new Map()
 
+function setUser(id,user){
+    sessionidUseMap.set(id,user)
+}
+
+function getUser(id){
+    return sessionidUseMap.get(id)
+}
+
+module.exports={
+    setUser,
+    getUser,
+}
 
 
 
